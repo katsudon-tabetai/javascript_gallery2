a82396259573c67e628c33e673b074ed86d75d94
katsudon-tabetai <131325371+katsudon-tabetai@users.noreply.github.com>
2024-10-22 10:34:31 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const items = document.querySelectorAll('.img-item');
+console.log(items);
+
+// for文
+for (let i = 0; i < items.length; i++) {
+
+    const keyframes = {
+        opacity: [0, 1],
+        translate: ['0 50px', 0],
+        filter: ['blur(10px)', 'blur(0)'],
+    };
+
+    const options = {
+        duration: 600,
+        delay: i * 600, // 600msごとに遅延
+        fill: 'forwards',
+    };
+
+    items[i].animate(keyframes, options);
+}
